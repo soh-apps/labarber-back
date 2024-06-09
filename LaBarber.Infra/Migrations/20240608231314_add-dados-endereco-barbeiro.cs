2a792a4393e1baa35422ece649ea810b4a8901c7
Lucas <lucas.macedo@domvsit.com.br>
2024-06-08 21:18:50 -0300

    feat: add migration endereco barbeiro

---
+++ LaBarber.Infra/Migrations/20240608231314_add-dados-endereco-barbeiro.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LaBarber.Infra.Migrations
+{
+    /// <inheritdoc />
+    public partial class adddadosenderecobarbeiro : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Complement",
+                table: "Barber",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Number",
+                table: "Barber",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Complement",
+                table: "Barber");
+
+            migrationBuilder.DropColumn(
+                name: "Number",
+                table: "Barber");
+        }
+    }
+}
